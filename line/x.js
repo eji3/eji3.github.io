77a374cf526f5cd77bf0e8fe0dc1514ab827d402
eji3 <154263269+eji3@users.noreply.github.com>
2024-02-20 17:44:43 +0800

    Add files via upload

---
+++ line/x.js
@@ -0,0 +1,19 @@
+﻿let delay = () => {
+    return new Promise((resolve, reject) => { setTimeout(resolve, 2000); });
+};
+
+async function p(e) {
+    let win = window.open('', 'p', 'width=100,height=100,left=' + e?.screenX + ',top=' + e?.screenY);
+    let list = document.querySelectorAll('p a:not([href=""])');
+    let len = list.length;
+    let cnt = 1;
+    for(let item of list) {
+        if (win.closed) break;
+        win.location.href = item.getAttribute("href");
+        await delay();
+        cnt++;
+        if (cnt > len) {
+            win.close();
+        }
+    }
+}
